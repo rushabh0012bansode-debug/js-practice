e34053095b5136d9f24e7f293bca9fdaab1f61b9
rushabh0012bansode-debug <rushabh0012bansode@gmail.com>
2026-06-03 17:05:07 +0530

    Add files via upload

---
+++ hello.js1.js
@@ -0,0 +1,11 @@
+// 1. VARIABLE
+const name = "viraj";
+// 2. FUNCTION
+function greet(person) {
+return "Hello, " + person + "! Welcome to Nexora.";
+}
+// 3. LOOP
+const students = ["viraj", "yash", "shriraj"];
+for (let i = 0; i < students.length; i++) {
+console.log( greet(students[i]) );
+}
